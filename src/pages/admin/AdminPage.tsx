1ddc8becc51dbd36b6533446b14117180f943f51
OhSSangHoon <gnsdkaos99@naver.com>
2024-05-31 22:10:46 +0900

    admin_playlist

--- src/pages/admin/AdminPage.tsx
+++ src/pages/admin/AdminPage.tsx
@@ -1,7 +1,41 @@
-const AdminPage = () =>{
-    return(
-        <></>
+import axios from 'axios';
+import React, { useState } from 'react';
+
+const AdminPage: React.FC = () => {
+    const [playlist, setPlaylist] = useState<string>('');
+
+    const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setPlaylist(event.target.value);
+    };
+
+    const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
+        event.preventDefault();
+        
+        try {
+            const response = await axios.post('http://localhost:8000/admin/music', {
+                playlist: playlist
+            });
+            if (response.status === 204) {
+                alert('Playlist가 성공적으로 업데이트 되었습니다.');
+            }
+        } catch (error) {
+            console.error('Playlist update error: ', error);
+            alert('Failed to update playlist.');
+        }
+    };
+
+    return (
+        <div>
+            <h1>Admin Page</h1>
+            <form onSubmit={handleSubmit}>
+                <label>
+                    Playlist:
+                    <input type="text" value={playlist} onChange={handleChange} />
+                </label>
+                <button type="submit">Update Playlist</button>
+            </form>
+        </div>
     );
-}
+};
 
 export default AdminPage;
